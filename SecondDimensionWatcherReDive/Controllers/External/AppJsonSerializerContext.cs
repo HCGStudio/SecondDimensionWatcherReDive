ed1e1d1a101f5a434bb8b20908f8409e99c1f1f7
mahoshojoHCG <hcgstd@proton.me>
2026-09-08 08:42:37 +0800

    fix: expose safe download capacity queue responses

--- SecondDimensionWatcherReDive/Controllers/External/AppJsonSerializerContext.cs
+++ SecondDimensionWatcherReDive/Controllers/External/AppJsonSerializerContext.cs
@@ -24,7 +24,7 @@ namespace SecondDimensionWatcherReDive.Controllers.External;
 [JsonSerializable(typeof(MetadataRecognitionRulePreview))]
 [JsonSerializable(typeof(MetadataRecognitionRuleSeed))]
 [JsonSerializable(typeof(MetadataRecognitionHistoryRequest))]
-[JsonSerializable(typeof(IReadOnlyList<DownloadCapacityEntry>))]
+[JsonSerializable(typeof(List<DownloadCapacityEntryResponse>))]
 [JsonSerializable(typeof(List<Feed>))]
 [JsonSerializable(typeof(IReadOnlyList<Feed>))]
 [JsonSerializable(typeof(Feed))]
